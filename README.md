31833000ace073ac0bcb94d2765adf6bd051c01c
weldson01 <weldsoncarlos007@gmail.com>
2026-02-16 21:39:38 -0300

    first commmit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Learning Laravel
+
+### Initial Setup
+
+1. Install PHP 8.3
+2. Install Composer
+3. Create a new Laravel project: `composer create-project laravel/laravel {your-project}`
+4. Install Laravel Sail: `php artisan sail:install`
+5. Configure Laravel Sail according to your preferences
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'WellApp') }}</title>
+    </head>
+    <body>
+        <h1>Bem-Vindo ao app do Well</h1>
+    </body>
+</html>
